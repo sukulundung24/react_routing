5fbd24fe056ebf3b8c7aabcd2f1fea1da02e695f
benison <sukulundung@gmail.com>
2017-06-02 22:23:21 +0800

    fixed routing

--- src/app/index.js
+++ src/app/index.js
@@ -7,6 +7,21 @@ require('./css/index.css');
 // Modules
 var TodoItem = require('./todoItem');
 var AddItem = require('./addItem');
+var About = require('./about');
+
+// Import
+import {Router, Route, browserHistory} from 'react-router';
+
+var App = React.createClass({
+	render: function(){
+		return(
+			<Router history={browserHistory}>
+				<Route path={'/'} component={TodoComponent}></Route>
+				<Route path={'/about'} component={About}></Route>
+			</Router>
+		);
+	}
+})
 
 //Create component
 var TodoComponent = React.createClass({
@@ -52,4 +67,4 @@ var TodoComponent = React.createClass({
 	}
 });
 
-ReactDOM.render(<TodoComponent/>,document.getElementById('todo-wrapper'));
+ReactDOM.render(<App/>,document.getElementById('todo-wrapper'));
